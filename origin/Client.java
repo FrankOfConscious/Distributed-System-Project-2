7f297513264a008e04bfb0da0524eea9bcbf3913
LiamZ95 <liamz95@outlook.com>
2017-05-20 14:10:39 +1000

    Add files via upload

---
+++ origin/Client.java
@@ -0,0 +1,250 @@
+package EZShare;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.logging.*;
+
+import org.apache.log4j.Logger;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import EZShare.Server;
+
+@SuppressWarnings("unused")
+class Client {
+	private static String ip;
+	private static int port;
+	private static boolean debug = false;	
+	private static final Logger log = Logger.getLogger(Logger.class);
+	public static void main(String[] args) {
+//		log.info("Client has started.");
+
+		// Parse CMD options
+		Options options = new Options();
+		AddOptions(options);
+
+		// accept args from CMD
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = null;
+
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			System.out.println("Command is invalid or not found. \nPlease check your command and try again.");
+			System.exit(0);
+		}
+		try{
+			port = Integer.parseInt(cmd.getOptionValue("port"));
+			if(port>65535||port<0){
+				System.out.println("Port or IP is invalid.\nPlease provid valid port and ip args.");
+				System.exit(0);
+			}
+			ip = cmd.getOptionValue("ip");
+			
+		}catch(Exception e){
+			log.warn("Port or IP is invalid.\nPlease provid valid port and ip args.");
+			System.exit(0);
+		}
+		if(cmd.hasOption("debug")) {
+			debug = true;
+		}
+		log.info("Client has started.");
+		// connect to a server socket
+		try (Socket socket = new Socket(ip, port)) {
+			// Get I/O streams for connection
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+			JSONObject raw=new JSONObject();
+			if (cmd.hasOption("fetch")) {
+//				JSONFetch(cmd, output, input);
+				raw.put("command", "FETCH");
+			}else if (cmd.hasOption("publish")) {
+//				JSONPublish(cmd, output);
+				raw.put("command", "PUBLISH");
+			} else if (cmd.hasOption("remove")) {
+//				JSONRemove(cmd, output);
+				raw.put("command", "REMOVE");
+			} else if (cmd.hasOption("share")) {
+//				JSONShare(cmd, output);
+				raw.put("command", "SHARE");
+			} else if (cmd.hasOption("exchange")) {
+//				JSONExchange(cmd, output);
+				raw.put("command", "EXCHANGE");
+			} else if (cmd.hasOption("query")) {
+//				JSONQuery(cmd, output);
+				raw.put("command", "QUERY");
+			}
+			
+			if(cmd.hasOption("channel")) raw.put("channel",cmd.getOptionValue("channel") );
+			if(cmd.hasOption("description")) raw.put("description",cmd.getOptionValue("description") );
+			if(cmd.hasOption("name")) raw.put("name",cmd.getOptionValue("name") );
+			if(cmd.hasOption("owner")) raw.put("owner",cmd.getOptionValue("owner") );
+			if(cmd.hasOption("secret")) raw.put("secret",cmd.getOptionValue("secret") );
+			if(cmd.hasOption("servers")) raw.put("servers",cmd.getOptionValue("servers") );
+			if(cmd.hasOption("tags")) raw.put("tags",cmd.getOptionValue("tags") );
+			if(cmd.hasOption("uri")) raw.put("uri",cmd.getOptionValue("uri") );
+			if(cmd.hasOption("relay")) raw.put("relay",cmd.getOptionValue("relay") );
+			try{
+				output.writeUTF(raw.toJSONString());
+				output.flush();
+				if(debug){
+					log.info("SENT: "+raw.toJSONString());	
+				}
+			}catch(IOException e){
+				e.printStackTrace();
+				System.exit(0);			
+			}
+			boolean fetchFlag=false;
+			try {
+				long startTime=System.currentTimeMillis();
+//				boolean timeoutFlag=true;
+				String fetchMessage = null;
+				while(true){
+					
+					if(input.available()>0){
+						startTime=System.currentTimeMillis();
+//						timeoutFlag=false;
+						String message = input.readUTF();
+						if(message.equals("{\"endOfTransmit\":true}")) break;
+						JSONParser parser1=new JSONParser();
+						if(debug){
+							log.info("RECEIVED: " + message);}
+						else{
+							log.info(message);}
+						if(((JSONObject) parser1.parse(message)).containsKey("resourceSize"))
+							fetchMessage=message;
+							
+//						if(debug){log.info("RECEIVED: " + message);}
+//						else{
+//							log.info(message);
+//						}
+					}
+					if((System.currentTimeMillis()-startTime)/1000>=5){
+						System.out.println("Time out.\nClient has exited.");
+						System.exit(0);
+					}
+				}
+				if(fetchMessage!=null) doFetch(fetchMessage,input);
+				input.close();
+				output.close();
+			} catch (IOException e) {
+					log.warn("Server seems to have closed connection.");
+					System.exit(0);
+				}
+		} catch (Exception e) {
+			log.warn("Server seems to have closed connection.");
+			System.exit(0);
+			//e.printStackTrace();			
+		}
+	}
+	
+	private static void doFetch(String result, DataInputStream input) {
+		// TODO Auto-generated method stub
+					
+				JSONObject cmd = new JSONObject();
+				try {
+					JSONParser parser = new JSONParser();;
+					cmd = (JSONObject) parser.parse(result);
+					// Create a RandomAccessFile to read and write the
+					// output file.
+					String uriStr = (String)cmd.get("uri");
+					String fileName = uriStr.substring( uriStr.lastIndexOf('/')+1, uriStr.length() );
+					RandomAccessFile downloadingFile = null;
+					try {
+						downloadingFile = new RandomAccessFile(fileName, "rw");
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					// Find out how much size is remaining to get from the
+					// server.					
+					long fileSizeRemaining = (Long) cmd.get("resourceSize");
+					int chunkSize = setChunkSize(fileSizeRemaining);
+					// Represents the receiving buffer
+					byte[] receiveBuffer = new byte[chunkSize];
+					// Variable used to read if there are remaining size
+					// left to read.
+					int num;
+					try {
+						while ((num = input.read(receiveBuffer)) > 0) {
+							// Write the received bytes into the
+							// RandomAccessFile
+							downloadingFile.write(Arrays.copyOf(receiveBuffer, num));
+
+							// Reduce the file size left to read..
+							fileSizeRemaining -= num;
+
+							// Set the chunkSize again
+							chunkSize = setChunkSize(fileSizeRemaining);
+							receiveBuffer = new byte[chunkSize];
+
+							// If you're done then break
+							if (fileSizeRemaining == 0) {
+								break;
+							}
+						}
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						downloadingFile.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} catch (org.json.simple.parser.ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}			
+		
+		
+	}
+
+	public static int setChunkSize(long fileSizeRemaining) {
+		// Determine the chunkSize
+		int chunkSize = 1024 * 1024;
+		// If the file size remaining is less than the chunk size
+		// then set the chunk size to be equal to the file size.
+		if (fileSizeRemaining < chunkSize) {
+			chunkSize = (int) fileSizeRemaining;
+		}
+		return chunkSize;
+	}
+
+	public static void AddOptions(Options options) {
+		options.addOption("port", true, "Server port");
+		options.addOption("ip", true, "Server IP address");
+		options.addOption("channel", true, "channel");
+		options.addOption("debug", false, "print debug information");
+		options.addOption("description", true, "resource description");
+		options.addOption("exchange", false, "exchange server list with server");
+		options.addOption("fetch", false, "fetch resources from server");
+		options.addOption("name", true, "resource name");
+		options.addOption("owner", true, "owner");
+		options.addOption("publish", false, "publish resource on server");
+		options.addOption("query", false, "query for resources from server");
+		options.addOption("remove", false, "remove resource from server");
+		options.addOption("secret", true, "secret");
+		options.addOption("servers", true, "server list");
+		options.addOption("share", false, "share resource on server");
+		options.addOption("tags", true, "resource tags");
+		options.addOption("uri", true, "resource URI");
+		options.addOption("relay", true, "query relay");
+	}
+
+}
---
+++ origin/Server.java
@@ -0,0 +1,339 @@
+package EZShare;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.net.ServerSocketFactory;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+	public class Server {
+		
+	
+		// Declare the port number
+		static int port;
+		//static String hostname;
+		// Identifies the user number connected
+		static String advertisedHostName;
+		private static int connectionIntervalLimit;
+		private static int exchangeInterval;
+		private static int counter = 0;
+		static boolean debug = false;
+		private static final Logger log = Logger.getLogger(Logger.class);
+		public static  ArrayList< KeyTuple> resourceList=new ArrayList<KeyTuple>();
+		static String secret = null;
+		static ArrayList<String> serverRecords=new ArrayList<String>();
+
+
+		
+		@SuppressWarnings("deprecation")
+		public static void main(String[] args) throws ParseException, org.apache.commons.cli.ParseException {
+			// Parse CMD options
+			//
+			Options options = new Options();
+			AddOptions(options);
+
+			// accept args from CMD
+			CommandLineParser parser = new DefaultParser();
+			CommandLine cmd = null;
+
+			try{
+				cmd = parser.parse(options, args);
+			}catch(Exception e){
+				System.out.println("Command is invalid or not found. \nPlease check your command and try again.");
+				System.exit(0);
+			}
+			try{
+				if(Math.isPort(cmd.getOptionValue("port")))
+					port = Integer.parseInt(cmd.getOptionValue("port"));
+				else{
+					System.out.println("Please provide valid port( integer:0~65535) arg.");
+					System.exit(0);
+				}
+			}catch(Exception e){
+				System.out.println("Please provide valid port( integer:0~65535) arg.");
+				System.exit(0);
+			}
+			
+			if(cmd.hasOption("connectionintervallimit")){
+				try{
+					Server.connectionIntervalLimit=Integer.parseInt(cmd.getOptionValue("connectionintervallimit"));
+					if(Integer.parseInt(cmd.getOptionValue("connectionintervallimit"))<0){
+						System.out.println("Please provide valid connection interval limit( positive integer) arg.");
+						System.exit(0);
+					}
+				}catch(Exception e){
+					System.out.println("Please provide valid connection interval limit( positive integer) arg.");
+					System.exit(0);
+				}
+			}else Server.connectionIntervalLimit=1;
+			if(cmd.hasOption("exchangeinterval")){
+				try{
+					Server.exchangeInterval=Integer.parseInt(cmd.getOptionValue("exchangeinterval"));
+					if(Integer.parseInt(cmd.getOptionValue("exchangeinterval"))<0){
+						System.out.println("Please provide valid exchange interval( positive integer) arg.");
+						System.exit(0);
+					}
+				}catch(Exception e){
+					System.out.println("Please provide valid exchange interval( postive integer) arg.");
+					System.exit(0);
+				}
+			}else Server.exchangeInterval=600;
+			if(cmd.hasOption("secret")){
+				try{
+					Server.secret=cmd.getOptionValue("secret");
+				}catch(Exception e){
+					System.out.println("Please provide valid secret(String).");
+					System.exit(0);
+				}
+			}else {
+				Random rand = new Random();
+				Server.secret=getRandomString(rand.nextInt(10)+20);
+			}
+			if(cmd.hasOption("advertisedhostname")){
+				try{
+					Server.advertisedHostName=cmd.getOptionValue("advertisedhostname");
+				}catch(Exception e){
+					System.out.println("Please provide valid advertised hostname(String).");
+					System.exit(0);
+				}
+			}else {
+				InetAddress gethost;
+				try {
+					gethost = InetAddress.getLocalHost();
+					Server.advertisedHostName=gethost.getHostName();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					System.out.println("Fail to get hostname of OS.\nTry to provide an advertised hostname manually.");
+				} 
+			}
+			if(cmd.hasOption("debug")) {
+				Server.debug=true;
+			}	
+			else debug=false;
+
+			
+			//////////////////////////////////////////////////////////////
+			ServerSocketFactory factory = ServerSocketFactory.getDefault();
+			try(ServerSocket server = factory.createServerSocket(port)){
+				log.info("Starting the EZshare Server");
+				log.info("using secret: "+Server.secret);
+				log.info("using advertiesd hostname: "+advertisedHostName);
+				log.info("bound to port "+port);
+				log.info("started");
+				
+				//***********************
+				Thread t2 = new Thread(() -> {
+					try {
+						exchangeServerRec();
+					} catch (UnknownHostException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				});
+				t2.start();
+				//**********************
+				// Wait for connections.
+				boolean connected = false;
+				long timeLimit = System.currentTimeMillis() + connectionIntervalLimit*1000;
+				
+				while(true){
+					Socket client = server.accept();
+					
+					if (System.currentTimeMillis() < timeLimit && connected) {
+						continue;
+					}
+					
+					counter++;
+					System.out.println("Client "+counter+": Applying for connection!");
+					// Start a new thread for a connection
+					Thread t = new Thread(() -> serveClient(client));
+					t.start();
+					
+					connected = true;
+					timeLimit = System.currentTimeMillis() + connectionIntervalLimit*1000;
+				}				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			
+		}
+		
+		
+
+		private static void serveClient(Socket client) {
+			try(Socket clientSocket = client){
+				
+				// The JSON Parser
+				JSONParser parser = new JSONParser();
+				// Input stream
+				DataInputStream input = new DataInputStream(clientSocket.
+						getInputStream());
+				// Output Stream
+			    DataOutputStream output = new DataOutputStream(clientSocket.
+			    		getOutputStream());
+//			    System.out.println("CLIENT: "+input.readUTF());
+//			    output.writeUTF("Server: Hi Client "+counter+" !!!");
+			    
+			    // Receive more data..
+		
+					
+					
+			    while(true){
+			    	if(input.available() > 0){
+			    		// Attempt to convert read data to JSON
+			    		JSONObject command = (JSONObject) parser.parse(input.readUTF());
+			    		if(debug){
+			    			log.debug("RECIEVED: "+command.toJSONString());
+			    		}
+//			    		if(Server.debug){
+//			    			log.debug("COMMAND RECEIVED: "+command.toJSONString());//////
+//			    		}else{
+//			    			System.out.println("COMMAND RECEIVED: "+command.toJSONString());
+//			    		}
+			    	
+			    		JSONArray result = Math.parseCommand(command, output);
+			    		for(int i=0;i<result.size();i++){
+				    		
+				    		output.writeUTF(((JSONObject)result.get(i)).toJSONString());
+				    		output.flush();	
+				    		if(debug&&!result.get(i).toString().equals("{\"endOfTransmit\":true}")){
+				    			log.debug("SENT: "+((JSONObject) result.get(i)).toJSONString());
+				    		}
+				    			
+			    		}
+			    		break;
+
+			    	}
+			    }
+			    
+			    output.close();
+	    		input.close();
+			    
+			} catch (IOException | ParseException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+		
+		////////to update
+		public static void AddOptions(Options options) {
+			options.addOption("debug", false, "Print debut information");
+			options.addOption("secret", true, "Server secret");
+			options.addOption("port", true, "server port, an integer");
+			options.addOption("exchangeinterval", true, "exchange interval in seconds");
+			options.addOption("connectionintervallimit", true, "connection interval limit in seconds");
+			options.addOption("advertisedhostname", true, "advertised hostname");
+			
+
+		}
+
+		private static String getRandomString(int length){
+		     String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		     Random random=new Random();
+		     StringBuffer sb=new StringBuffer();
+		     for(int i=0;i<length;i++){
+		       int number=random.nextInt(62);
+		       sb.append(str.charAt(number));
+		     }
+		     return sb.toString();
+		 }
+		
+		//********************
+		private static void exchangeServerRec() throws UnknownHostException, IOException {
+			
+			
+			// Creates a socket for another server, the socket that will send msg to
+
+			Timer timer = new Timer();
+			TimerTask task = new TimerTask() {
+
+				public void run() {
+					// TODO Auto-generated method stub
+					if (serverRecords.size() == 0) {
+						
+					} else {
+						
+						int selectedIndex = (new Random()).nextInt(serverRecords.size());
+					
+						String host_ip = serverRecords.get(selectedIndex);
+						String[] host_ip_arr = host_ip.split(":");
+						String host_name = host_ip_arr[0];
+						int ip_add = Integer.parseInt(host_ip_arr[1]);
+						JSONObject exchangeCommand = new JSONObject();
+						String records = "";
+						for (int i = 0; i<serverRecords.size(); i++) {
+							records += serverRecords.get(i) + ",";
+						}
+						try {
+							records += InetAddress.getLocalHost().getHostAddress() + ":" + port;
+						} catch (UnknownHostException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						exchangeCommand.put("command", "EXCHANGE");
+						exchangeCommand.put("serverList", records);
+						
+						try(Socket randomServer = new Socket(host_name, ip_add)){
+							DataInputStream input = new DataInputStream(randomServer.getInputStream());
+							DataOutputStream output = new DataOutputStream(randomServer.getOutputStream());
+						
+							output.writeUTF(exchangeCommand.toJSONString());
+							output.flush();
+							
+							//System.out.println("Command sent");
+							
+							// Time limit for execution
+							long start = System.currentTimeMillis();
+							long end = start + 5 * 1000;
+							boolean isReachable = false;
+							while(System.currentTimeMillis() < end) {
+								if (input.available() > 0) {
+									isReachable = true;
+									String result = input.readUTF();
+								
+								}
+							}
+							if (!isReachable) {
+								serverRecords.remove(selectedIndex);
+								//System.out.println("Removed unreachable server-" + serverRecords.get(selectedIndex));
+							}
+							
+						} catch (IOException e) {
+							//e.printStackTrace();
+							//System.out.println("Record invalid!" + serverRecords.size());
+							serverRecords.remove(selectedIndex);
+						}
+					}
+				}
+				
+			};
+			timer.schedule(task, 0, exchangeInterval * 1000);
+		}
+
+	}
